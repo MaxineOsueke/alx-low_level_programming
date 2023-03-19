4b1ac67b4a0f999e8489edf33e9c1a556a2879ef
MaxineOsueke <maxineosueke@gmail.com>
2023-03-19 13:20:03 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
