7c33ee672c27fd3c6b55d6472543108316d1107f
MaxineOsueke <maxineosueke@gmail.com>
2023-03-19 12:16:02 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Program that prints the lowercase alphabet in reverse
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
