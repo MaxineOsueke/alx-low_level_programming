b47b81b93fa5e6f65feab5de508200cbf561b57c
MaxineOsueke <maxineosueke@gmail.com>
2023-03-23 06:26:40 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
+
 /**
-* main - causes an infinite loop
-* Return: 0
+ * main - causes an infinite loop
+ * 0x - The number to be checked
+ * Return: 1 for character that will be a digit or 0 for others
 */
 int main(void)
 {
-int i;
-printf("Infinite loop incoming :(\n");
-i = 0;
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
 }
-
